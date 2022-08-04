521c13c1caaeb6ccfb97ea269126f7a01e1ed9a3
Sergey <zapravilsj@mail.ru>
2022-08-04 15:05:33 +0500

    ДЗ готово

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    String title;
+    int yearOfPublication;
+    Author author;
+
+    public Book(Author author, String title, int yearOfPublication) {
+        this.title = title;
+        this.yearOfPublication = yearOfPublication;
+        this.author = author;
+    }
+
+    public Author getAuthor() {
+        return  this.author;
+    }
+    public String getTitle () {
+        return  this.title;
+    }
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+    public void setYearOfPublication(int yearOfPublication ) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Author nikolayGogol = new Author("Николай","Гоголь");
+        Book bookOne = new Book(levTolstoy, "Война и мир", 2000);
+        Book bookTwo = new Book(nikolayGogol, "Мертвые души", 1966);
+        System.out.println("bookOne.author.toString() = " + bookOne.author.toString());
+        System.out.println("bookOne.yearOfPublication = " + bookOne.yearOfPublication);
+        bookOne.setYearOfPublication(1960);
+        System.out.println("bookOne.yearOfPublication = " + bookOne.yearOfPublication);
+
     }
 }
